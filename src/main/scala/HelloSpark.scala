030b6ad999b7790d0e1277bb3a6616cba7d8082d
David Lauzon <davidonlaptop@gmail.com>
2015-10-28 06:48:18 -0400

    Compiling, but throws runtime errors

--- src/main/scala/HelloSpark.scala
+++ src/main/scala/HelloSpark.scala
@@ -4,16 +4,19 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
-object HelloSpark {
-  def main(args: Array[String]) {
-    //val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
-    //*
+object HelloSpark
+{
+  def main(args: Array[String])
+  {
     val logFile = args(0)
-    val conf = new SparkConf().setAppName("Simple Application")
-    val sc = new SparkContext(conf)
+
+    // Spin-off a local Spark cluster
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+    val sc   = new SparkContext(conf)
+
     val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
+    val numAs   = logData.filter(line => line.contains("a")).count()
+    val numBs   = logData.filter(line => line.contains("b")).count()
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
     // */
     println("success spark scala")
